c05c64f3f81d3dda5a0b3bb208e2b4e903880205
DESKTOP-EE6KA0I\REDTABLE <kkj214@gmail.com>
2022-11-10 17:45:08 +0900

    Scala 공부

--- 공부/Scala/Scala_1_변수&계산.scala
+++ 공부/Scala/Scala_1_변수&계산.scala
@@ -1,4 +1,4 @@
-// Scala 기초 문법 공부
+// # Scala 기초 문법 공부_1
 
 // Hello, world!
 object LearnScala {
@@ -73,36 +73,4 @@ object LearnScala {
 
 /*
 println, printf 둘다 사용 가능 , 출력 문법은 간결해서 편리해보임
-*/
-
-
-// Range & List
-object LearnScala {
-    def main(args: Array[String]): Unit = {
-        // ① to를 이용하면 1부터 10을 포함하는 Range를 생성
-        val range1 = 1 to 10
-        println(s"① 1 to 10 →\n\t $range1")
-        
-        // ② until을 이용하면 마지막 숫자를 포함하지 않는 Range를 생성
-        val range2 = 1 until 10
-        println(s"② 1 until 10 →\n\t $range2")
-        
-        // ③ by를 이용하면 숫자를 건너 띄는 Range를 생성
-        val range3 = 1 until 10 by 3
-        println(s"③ 1 until 10 by 3 →\n\t $range3")
-        
-        // ④ toList를 통해 List로 변환
-        println(s"④ range1.toList →\n\t ${range1.toList}")
-        
-        // ⑤ filter: 조건에 맞는것만 모으기(4 이상인것만 모으기)
-        val moreThan4 = range1.filter(_ > 4)  
-        println(s"⑤ range1.filter(_ > 4) →\n\t $moreThan4")
-        
-        // ⑥ map - 각 아이템의 값을 변경
-        val doubleIt = range1.map(_ * 2)  
-        println(s"⑥ range1.map(_ * 2) →\n\t $doubleIt")
-    }
-}
-/*
-to, until, by 라는 편리한 기능을 제공해주고, range의 filter와 map기능도 유용하게 쓸 수 있을것 같음
 */
